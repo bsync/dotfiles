cae075818517057e0f32e1a375887a7c47062af3
James Travis Horine <james.horine@gmail.com>
2024-11-28 05:03:38 +0000

    Add some consistency in feedback

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if command -v nix &>/dev/null; then
-  echo "Nix is already installed. Skipping installation."
+  echo "nix is already installed. Skipping installation."
 else
   curl -L https://nixos.org/nix/install | sh
 fi
 
 NIX_CONF=$HOME/.config/nix/nix.conf
 if [[ -f $HOME/.config/nix/nix.conf ]]; then
-  echo "Nix configuration detected at $NIX_CONF"
+  echo "nix configuration already exists at $NIX_CONF. Skipping configuration."
 else
   echo "Configuring Nix for single-user mode..."
   # Ensure .config/nix directory exists
@@ -19,21 +19,21 @@ experimental-features = nix-command flakes
 EOT
 fi
 
-if ! nix-channel --list | grep -q home-manager; then
+if nix-channel --list | grep -q home-manager; then
+  echo "Channel for home-manager already exists; skipping channel update."
+else
   nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   nix-channel --update
-else
-  echo "Channel for home-manager detected."
 fi
 
 if command -v home-manager &>/dev/null; then
-  echo "Home manager detected."
+  echo "home-manager already exists; skipping installation."
 else
   nix-shell '<home-manager>' -A install
 fi
 
 if [[ -d $HOME/.config/home-manager ]]; then
-  echo "Home manager config already exists."
+  echo "home-manager config already exists; skipping configuration."
 else
   ln -s $(realpath $(dirname $0)/home-manager) $HOME/.config/home-manager
 fi
