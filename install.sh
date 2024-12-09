5bc347b7cdff75cb0c957a56cb38d9c2efc9c6c5
travis <jhorine@silverthreadinc.com>
2024-12-08 23:39:35 -0600

    Install neovim appimage and my personal config with install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-ln -s $(realpath .config/nvim) ~/.config/nvim
+SPATH=$(dirname $(realpath $BASH_SOURCE[0]))
+[[ -L ~/.config/nvim ]] || ln -s ${SPATH}/.config/nvim ~/.config/nvim
+[[ -f ${SPATH}/nvim ]] || wget -O - https://github.com/neovim/neovim/releases/download/v0.10.2/nvim.appimage > ${SPATH}/nvim
+chmod u+x ${SPATH}/nvim
+alias nvim=${SPATH}/nvim
