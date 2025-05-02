7bb2155b8d9d4985a34353e4cd18265f717b3817
travis <jhorine@silverthreadinc.com>
2025-05-02 08:04:09 -0500

    use parrot.nvim for ai interface.

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,57 +1,15 @@
 return {
-	"olimorris/codecompanion.nvim",
-	config = true,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	opts = {
-		strategies = {
-			-- Change the default chat adapter
-			chat = {
-				adapter = "anthropic",
+	"frankroeder/parrot.nvim",
+	dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
+	-- optionally include "folke/noice.nvim" or "rcarriga/nvim-notify" for beautiful notifications
+	config = function()
+		require("parrot").setup({
+			-- Providers must be explicitly added to make them available.
+			providers = {
+				anthropic = {
+					api_key = { "gopass", "show", "-o", "api/anthropic/key" },
+				},
 			},
-			inline = {
-				adapter = "anthropic",
-			},
-		},
-		adapters = {
-			anthropic = function()
-				return require("codecompanion.adapters").extend("anthropic", {
-					env = {
-						api_key = "cmd:gopass api/anthropic/key",
-					},
-				})
-			end,
-		},
-	},
-	keys = {
-		{
-			"<C-a>",
-			"<cmd>CodeCompanionActions<cr>",
-			mode = { "n", "v" },
-			desc = "Code Companion Actions",
-			noremap = true,
-			silent = true,
-		},
-		{
-			"<LocalLeader>a",
-			"<cmd>CodeCompanionChat Toggle<cr>",
-			mode = { "n", "v" },
-			desc = "Toggle Code Companion Chat",
-			noremap = true,
-			silent = true,
-		},
-		{
-			"ga",
-			"<cmd>CodeCompanionChat Add<cr>",
-			mode = "v",
-			desc = "Add to Code Companion Chat",
-			noremap = true,
-			silent = true,
-		},
-	},
-	init = function() -- Using init to execute it right away
-		vim.cmd([[cab cc CodeCompanion]])
+		})
 	end,
 }
