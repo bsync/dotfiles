266bf823dcff501b8bd82f62a0d095aa58848def
travis <jhorine@silverthreadinc.com>
2025-05-12 08:51:21 -0500

    Try lazyvim for a while.

--- .config/nvim/lua/plugins/autosession.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"rmagatti/auto-session",
-	config = function()
-		require("auto-session").setup({
-			auto_restore = false,
-			suppressed_dirs = { "~/", "/" },
-		})
-	end,
-}
--- .config/nvim/lua/plugins/db.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"tpope/vim-dadbod",
-	"kristijanhusak/vim-dadbod-completion",
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		keys = { { "<leader>D", "<cmd>DBUIToggle<CR>", desc = "Hide/Show Database UI" } },
-	},
-}
