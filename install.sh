33db48362bdc9c06ef862bb0aa957210b3698f22
James Travis Horine <james.horine@gmail.com>
2025-09-04 14:28:43 -0500

    Update to use neovim 0.11.3

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 SPATH=$(dirname $(realpath $BASH_SOURCE[0]))
 [[ -d ~/.config ]] || mkdir -p ~/.config
 [[ -L ~/.config/nvim ]] || ln -s ${SPATH}/config/nvim ~/.config/nvim
-[[ -f ${SPATH}/nvim ]] || wget -O - https://github.com/neovim/neovim/releases/download/v0.10.2/nvim.appimage >${SPATH}/nvim
+[[ -f ${SPATH}/nvim ]] || wget -O - https://github.com/neovim/neovim-releases/releases/download/v0.11.3/nvim-linux-x86_64.appimage >${SPATH}/nvim
 chmod u+x ${SPATH}/nvim
 alias nvim=${SPATH}/nvim
